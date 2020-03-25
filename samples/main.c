8679844918f3b6cb69539331fbeb713984a0687a
quantumcore <fastcorp11@gmail.com>
2020-03-26 01:38:23 +0500

    Sample fixed, fix expire feature

--- samples/main.c
+++ samples/main.c
@@ -1,8 +1,15 @@
 #include "xkeyverify.h"
 
+#define ALL_OK "12345" // Use encryption if use in real application
+
 int main() {
-    char* key[MAX_KEY]; memset(key, '\0', MAX_KEY);
-    get_key("localhost", "mainkey", key);
-    printf("KEY : %s\n", key);
+    char key[MAX_KEY]; memset(key, '\0', MAX_KEY);
+    int test = get("http://localhost/mainkey", key, MAX_KEY);
+    if(strcmp(key, ALL_OK) == 0)
+    {
+        printf("Key : %s\n", key);
+    } else {
+        MessageBoxA(NULL, "Your Trial has expired.", "Error", MB_ICONERROR);
+    }
     return 0;
 }
--- samples/xkeyverify.h
+++ samples/xkeyverify.h
@@ -4,43 +4,29 @@
 #include <winsock2.h>
 #include <stdio.h>
 #include <windows.h>
+#include <wininet.h>
 
 #define MAX_KEY 1024
 
-int get_key(const char* url, char* key, char keybuf[MAX_KEY])
-{
-    WSADATA wsa; struct sockaddr_in server; SOCKET s;
-    struct hostent* host;
-    char* request[256]; char reply[1024];
-    if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
-    {
-        return WSAGetLastError();
-    }
-    host = gethostbyname(url);
-    server.sin_addr.s_addr = inet_addr(host->h_addr);
-    server.sin_port = htons(80);
-    server.sin_family = AF_INET;
+// https://gist.github.com/augustgl/38d5d849af99fe78063dbfebdce7c153
+int get(char *szUrl, char *recv_data, DWORD recv_size) {
+	DWORD NumberOfBytesRead = 0;
+	RtlZeroMemory(recv_data, recv_size);
 
-    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(s == INVALID_SOCKET || s == SOCKET_ERROR)
-    {
-        return WSAGetLastError();
-    }
+	HINTERNET connect = InternetOpen("browser", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
 
-    if(connect(s, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR){
-        return WSAGetLastError(); 
-    } else {
-        memset(request, '\0', 256); memset(reply, '\0', MAX_KEY); memset(keybuf, '\0', MAX_KEY);
-        snprintf(request, 256, "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", key, url);
-        send(s, request, 256, 0);
-        int rp = recv(s, reply, 1024, 0);
-        if(rp != SOCKET_ERROR){
-            snprintf(keybuf, MAX_KEY, "%s", reply);
-        } else {
-            return WSAGetLastError();
-        }
+	if (connect) {
+		HINTERNET openAddr = InternetOpenUrl(connect, szUrl, NULL, 0, INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_KEEP_CONNECTION, 0);
 
-    }
+		if (openAddr) {
+			InternetReadFile(openAddr, recv_data, recv_size, &NumberOfBytesRead);
+			InternetCloseHandle(connect);
+			InternetCloseHandle(openAddr);
+			return NumberOfBytesRead;
+		}
+		InternetCloseHandle(connect);
+	}
+	return -1;
 }
 
 #endif
